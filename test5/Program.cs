de261b3cf2c90a6adcae1c12e72fc29042b3913f
PandBonf <pandbon@gmail.com>
2022-03-05 20:32:24 +0300

    test5

---
+++ test5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace test5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TransformToElephant();
+            Console.WriteLine("Муха");
+
+        }
+
+
+        static void TransformToElephant()
+        {
+            Thread thread = new Thread(() =>
+            {
+                Thread.Sleep(100);
+                Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+                Console.WriteLine("Слон");
+            });
+
+            thread.Start();
+        }
+    
+
+    }
+}
